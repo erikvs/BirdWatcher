3bfb387a5ab5b0b6315bc01fd5dec08c209854ff
erikvs <89622099+erikvs@users.noreply.github.com>
2022-10-03 20:01:05 +0200

    Initial commit

---
+++ app/src/main/java/com/gruppe16/birdwatcher/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gruppe16.birdwatcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.birdwatcher.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
